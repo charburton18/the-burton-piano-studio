0d0bad55080e756fd53c60955754b0a44603ac62
Char B <charburton18@gmail.com>
2024-06-21 16:08:05 -0700

    tailwind is up and running :)

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,11 +2,11 @@ function NavBar() {
 
   return (
     <>
-      <nav class=".bg-black">
-        <a>Home</a>
-        <a>About</a>
-        <a>Contact</a>
-        <a>Login/Register</a>
+      <nav className="bg-black p-5 flex justify-between px-10 outline outline-gray-500 outline-1">
+        <a className="text-white">Home</a>
+        <a className="text-white">About</a>
+        <a className="text-white">Contact</a>
+        <a className="text-white">Login/Register</a>
       </nav>
     </>
   )
