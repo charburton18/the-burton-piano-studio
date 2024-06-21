f0bacdfcc27ecc685ed6809596613197fbc5b0ce
Char B <charburton18@gmail.com>
2024-06-21 13:54:47 -0700

    reverted project into Vite template

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import NavBar from "./Components/NavBar"
+
+function App() {
+
+  return (
+    <>
+      <NavBar></NavBar>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,15 @@
+function NavBar() {
+
+  return (
+    <>
+      <nav class=".bg-black">
+        <a>Home</a>
+        <a>About</a>
+        <a>Contact</a>
+        <a>Login/Register</a>
+      </nav>
+    </>
+  )
+}
+
+export default NavBar
