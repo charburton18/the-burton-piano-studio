a0e3ab65736349234ea7c0f347fbb8cebeed8109
Char B <charburton18@gmail.com>
2024-07-05 12:38:00 -0700

    installed react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import NavBar from "./Components/NavBar"
 import Home from "./Pages/Home"
+import {Routes, Route} from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-      <NavBar></NavBar>
-      <Home></Home>
+      <Routes>
+        <NavBar></NavBar>
+        <Route path='/' element={<Home></Home>} />
+      </Routes>
     </>
   )
 }
