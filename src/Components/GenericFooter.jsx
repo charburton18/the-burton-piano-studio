32e7d78714692acdb288560793a16562e5a58136
Char B <charburton18@gmail.com>
2024-08-20 08:03:38 -0700

    added GenericFooter.jsx with email and fb images

---
+++ src/Components/GenericFooter.jsx
@@ -0,0 +1,15 @@
+import {Link} from 'react-router-dom'
+
+function GenericFooter() {
+
+  return (
+    <>
+      <footer className="flex justify-center">
+        <img className="h-24 p-2" src='piano-studio-images/email-icon.png'></img>
+        <img className="h-24 p-2" src='piano-studio-images/fb-icon.png'></img>
+      </footer>
+    </>
+  )
+}
+
+export default GenericFooter
