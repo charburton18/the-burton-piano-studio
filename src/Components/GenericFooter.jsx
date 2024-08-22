494a589ef7ff67223468c09ad574832d15a93829
Char B <charburton18@gmail.com>
2024-08-22 14:59:04 -0700

    cleared footer content

--- src/Components/GenericFooter.jsx
+++ src/Components/GenericFooter.jsx
@@ -4,10 +4,10 @@ function GenericFooter() {
 
   return (
     <>
-      <footer className="flex justify-center">
+      {/* <footer className="flex justify-center">
         <img className="h-24 p-2" src='piano-studio-images/email-icon.png'></img>
         <img className="h-24 p-2" src='piano-studio-images/fb-icon.png'></img>
-      </footer>
+      </footer> */}
     </>
   )
 }
