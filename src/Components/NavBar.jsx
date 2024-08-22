b14228072d912ee08c9c29a4571b1c220ac2f3ae
Char B <charburton18@gmail.com>
2024-08-22 15:30:06 -0700

    removed outline and increased font size on navbar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -4,11 +4,13 @@ function NavBar() {
 
   return (
     <>
-      <nav className="bg-zinc-900 p-5 flex justify-between px-10 outline outline-gray-500 outline-1">
-        <Link to="/"><p className="text-zinc-200">Home</p></Link>
-        <Link to="/about"><p className="text-zinc-200">About</p></Link>
-        <Link to="/contact"><p className="text-zinc-200">Contact</p></Link>
-        <Link to="/login-register"><p className="text-zinc-200">Login/Register</p></Link>
+      <nav className="bg-zinc-900 p-5 flex justify-center px-10 h-100">
+        <div className="flex justify-between w-1/2">
+          <Link to="/"><p className="text-zinc-200 text-4xl font-thin">Home</p></Link>
+          <Link to="/about"><p className="text-zinc-200 text-4xl font-thin">About</p></Link>
+          <Link to="/contact"><p className="text-zinc-200 text-4xl font-thin">Contact</p></Link>
+          <Link to="/login-register"><p className="text-zinc-200 text-4xl font-thin">Login</p></Link>
+        </div>
       </nav>
     </>
   )
